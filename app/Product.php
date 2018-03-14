68fdb6bd35f27c3bda3e1d5d15df38be64a0dad3
Manson Xasthur <mansonx13@gmail.com>
2018-03-14 14:41:37 +0200

    remove taglines

--- app/Product.php
+++ app/Product.php
@@ -16,19 +16,6 @@ class Product extends GMS
         'parent_company_id' => 1
     ];
 
-    /*
-     * Constant value for Arabic tagline
-     *
-     * @var string
-     * */
-    const AR_TAGLINE = "منتجاتنا تعتمد على بناء وتوفير مستوى عال من خدمات المحتوى الرقمى";
-
-    /*
-     * Constant value for English tagline
-     *
-     * @var string
-     * */
-    const EN_TAGLINE = "Our products are a variety of phone applications and value added services and digital marketing all of this in GMS Group relies on building and providing a high level of technology";
 
     /*
      * Assign a relation with Client class
